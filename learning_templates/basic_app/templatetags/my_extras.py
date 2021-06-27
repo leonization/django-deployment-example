50473d16e241b83f826824fdbac2538f9b6b53f5
Shaurya Pratap Singh <76516781+leonization@users.noreply.github.com>
2021-06-27 13:46:19 +0530

    Update my_extras.py

--- learning_templates/basic_app/templatetags/my_extras.py
+++ learning_templates/basic_app/templatetags/my_extras.py
@@ -3,7 +3,7 @@ from django import template
 register = template.Library()
 
 @register.filter(name="cut" )
-def cut(value'arg):
+def cut(value, arg):
     """
     this cuts out all values of "arg" from the string
     """
